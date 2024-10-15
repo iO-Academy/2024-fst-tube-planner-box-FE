58b36fa91236e33950ee4d41ede44320b9c526bd
Joseph Padfield <joseph.padfield@gmail.com>
2024-10-15 11:38:35 +0100

    results to the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {useEffect, useState} from "react";
 import Form from "./components/organisms/Form/index.jsx";
 import H2Element from "./components/atoms/H2Element/index.jsx";
 import Header from "./components/molecules/Header/index.jsx";
+import Results from "./components/organisms/Results/index.jsx";
 
 function App() {
 
@@ -26,6 +27,7 @@ function App() {
         <Header />
         <H2Element content="Where are you going, mate?"/>
         <Form />
+        <Results numStops="numStops" line="line"/>
     </>
   )
 }
