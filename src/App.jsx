f09c980fb3a49a70fec0dc9d2968f954a5f7bfb3
Thomas King <thomasgking42@gmail.com>
2024-10-15 10:43:25 +0100

    fetching to BE

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css'
+import {useEffect, useState} from "react";
 
 function App() {
+
+    const [data, setData] = useState([])
+    const [query, setQuery] = useState('http://localhost:3000')
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await fetch(query)
+            const tubes = await response.json()
+            setData(tubes)
+        }
+        getData()
+    }, [query]);
+
+    console.log(data)
 
 
   return (
     <>
         <h1>Welcome to JourneyBox!</h1>
+
     </>
   )
 }
