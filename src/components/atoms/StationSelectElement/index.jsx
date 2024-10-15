37417cb09d0cebfbc4a3f700e2439c2e09ad85a9
JosephPadfield <josephpadfield@josephpieldsmbp.home>
2024-10-15 23:33:07 +0100

    dropdown problem solved - selecting from one removes from other

--- src/components/atoms/StationSelectElement/index.jsx
+++ src/components/atoms/StationSelectElement/index.jsx
@@ -1,30 +1,27 @@
 import StationOptionElement from "../StationOptionElement/index.jsx";
 import {useEffect, useState} from "react";
 
-const StationSelectElement = ({name, id}) => {
+const StationSelectElement = ({data, name, id, selectedStation, setSelectedStation}) => {
 
-    const [data, setData] = useState([])
-    const [query, setQuery] = useState('http://localhost:3000')
-
-    useEffect(() => {
-        const getData = async () => {
-            const response = await fetch(query)
-            const tubes = await response.json()
-            setData(tubes)
-        }
-        getData()
-    }, [query]);
+    console.log(data)
 
     return (
-        <select name={name} id={id} className="border border-gray-300">
-            <option value="none" selected disabled hidden>Select</option>
+        <select
+            name={name}
+            id={id}
+            className="border border-gray-300"
+            value={selectedStation}
+            onChange={(e) => setSelectedStation(e.target.value)}>
+
+            <option value="" disabled hidden> Select</option>
             {
-                data.map((tube) => {
-                    return (
-                        <StationOptionElement key={tube.code} value={tube.name + ' ' + tube.code}/>
-                    )
-                })
-            }
+                data.length > 0 ? (
+                    data.map((tube) => (
+                        <StationOptionElement
+                            key={tube.code + tube.line + tube.name}
+                            value={tube.name + ' ' + tube.code}/>
+                    ))
+                ) : <option disabled>Loading...</option>}
         </select>
     )
 }
--- src/components/organisms/Form/index.jsx
+++ src/components/organisms/Form/index.jsx
@@ -1,22 +1,39 @@
 import LabelElement from "../../atoms/LabelElement/index.jsx";
 import StationSelectElement from "../../atoms/StationSelectElement/index.jsx";
 import SubmitButtonElement from "../../atoms/SubmitButtonElement/index.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const Form = () => {
 
-    const [fromStation, setFromStation] = useState("")
-    const [toStation, setToStation] = useState("")
+    const [data, setData] = useState([])
+    const [query, setQuery] = useState('http://localhost:3000')
+    // const [fromStation, setFromStation] = useState("")
+    // const [toStation, setToStation] = useState("")
+    const [selectedStation, setSelectedStation] = useState('');
+
+    const filteredData = data.filter(item => item.name + ' ' + item.code !== selectedStation)
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await fetch(query)
+            const tubes = await response.json()
+            setData(tubes)
+        }
+        getData()
+    }, []);
+
 
     return (
         <form className="border-2 border-solid border-black rounded-2xl flex flex-col p-4 bg-white m-4 shadow-2xl">
-            <LabelElement inputName="from" htmlFor="from" />
-            <StationSelectElement name="from" id="from"  selectedStation={fromStation}
-                                  setSelectedStation={setFromStation}
-                                  otherSelectedStation={toStation} />
-            <LabelElement inputName="to" htmlFor="to" />
-            <StationSelectElement name="to" id="to" />
-            <SubmitButtonElement />
+            {/*<LabelElement inputName="from" htmlFor="from" />*/}
+            <StationSelectElement data={data} name="from" id="from" selectedStation={selectedStation}
+                                  setSelectedStation={setSelectedStation}/>
+            <p>Selected station = {selectedStation}</p>
+            {/*<LabelElement inputName="to" htmlFor="to" />*/}
+            <StationSelectElement data={filteredData} name="to" id="to" selectedStation="placeholder"
+                                  setSelectedStation={setSelectedStation}/>
+            {/*<StationSelectElement name="to" id="to" />*/}
+            {/*<SubmitButtonElement />*/}
         </form>
 
 )
